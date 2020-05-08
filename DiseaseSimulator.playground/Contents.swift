77b359f115b05abf7deb94100eea0283f830e313
Rafael Ferreira <rafaelfca.ferreira@gmail.com>
2020-05-08 17:09:15 -0300

    first commit

---
+++ DiseaseSimulator.playground/Contents.swift
@@ -0,0 +1,6 @@
+import PlaygroundSupport
+import UIKit
+
+let board = initiateBoard()
+
+PlaygroundPage.current.liveView = board
---
+++ DiseaseSimulator.playground/Sources/Agents.swift
@@ -0,0 +1,37 @@
+import Foundation
+import UIKit
+
+public class Agents: UIButton {
+    var position: (Int,Int)
+    var neighbours: [(line: Int,column: Int)]
+    var boardSize: (nLines: Int, nColumns: Int)
+    
+    public init(frame: CGRect, position: (Int,Int), boardSize: (Int, Int)) {
+        self.position = position
+        self.boardSize = boardSize
+        self.neighbours = []
+        super.init(frame: frame)
+        self.neighbours = self.findNeighbours(position: position)
+    }
+    
+    // default required initializer
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func findNeighbours(position: (line: Int,row: Int)) -> [(Int,Int)] {
+        var validNeighbours: [(Int, Int)] = []
+        for line in (position.line - 2)...(position.line) { //checking the lines above and below the current cell
+            if line >= 0 && line < boardSize.nLines { //making sure the line is valid
+                for row in (position.row - 2)...(position.row) { //checking the rows before and after the current cell
+                    if row >= 0 && row < boardSize.nColumns { //checking that the row is a valid one
+                        if line != position.line-1 || row != position.row-1 {
+                            validNeighbours.append((line, row))
+                        }
+                    }
+                }
+            }
+        }
+        return validNeighbours
+    }
+}
